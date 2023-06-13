ec3679a7834783268e3925b236f3c83d19df8b56
josefervalenciacontreras <fernando.valencia@est.iudigital.edu.co>
2023-06-13 16:07:19 -0500

    agregando carpeta templates

---
+++ template/cabecera.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Carrito</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tienda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Productos.php">Productos</a>
+            </li>
+            
+        </ul>
+    </nav>
+    <div class="container">
+        <div class="row">
