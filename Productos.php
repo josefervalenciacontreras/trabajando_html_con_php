df1c5d0c87c2e18495f67fc052ec84c81232d819
josefervalenciacontreras <fernando.valencia@est.iudigital.edu.co>
2023-06-13 16:02:16 -0500

    agregando productos

---
+++ Productos.php
@@ -0,0 +1,45 @@
+<?php include("template/cabecera.php")?>
+<div class="jumbotron">
+    <h1 class="display-3">Productos</h1>
+    <p class="lead">Bienvenidos a la tienda virtual de cacharreros.</p>
+    <hr class="my-2">
+    <p>En esta pagina encontrarar todos los productos disponibles de nuestra tienda</p>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./imagenes/FotosCatalogo/5045-negro-1.jpg" alt="" style="width: 200px">
+    <div class="card-body">
+        <h4 class="card-title">Gaban Negro</h4>
+        <p class="card-text">En algodon</p>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./imagenes/FotosCatalogo/5047-cafe-mostaza.jpg" alt="" style="width: 200px">
+    <div class="card-body">
+        <h4 class="card-title">Gaban Negro</h4>
+        <p class="card-text">En algodon</p>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./imagenes/FotosCatalogo/5046-berenjena1-1.jpg" alt="" style="width: 200px">
+    <div class="card-body">
+        <h4 class="card-title">Gaban Negro</h4>
+        <p class="card-text">En algodon</p>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./imagenes/FotosCatalogo/7570-curuba-nylon-1-20220407-185849.jpg" alt="" style="width: 200px">
+    <div class="card-body">
+        <h4 class="card-title">Gaban Negro</h4>
+        <p class="card-text">En algodon</p>
+    </div>
+</div>
+</div>
+
+<?php include("template/pie.php")?>
